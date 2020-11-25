1ed67f9092929e8d132d992ba67855c9d45c177a
侯光辉 <ethan.hou@inno-medi.com>
2020-11-25 14:54:03 +0800

    Init Commit

--- src/main/java/com/alipay/alipaydemo/config/AlipayConfig.java
+++ src/main/java/com/alipay/alipaydemo/config/AlipayConfig.java
@@ -13,7 +13,25 @@ import java.util.Map;
 @Configuration
 public class AlipayConfig {
 
+    public static String sign_type = "RSA2";
+
+    // 字符编码格式
+    public static String charset = "utf-8";
+
+    public static String alipay_public_key = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAhuffPhQ1NxQZX3X8PGt3" +
+            "qIA9Z8wexed2Lrj4ssHvt2fLcjVKr5MQ9GP4hEZG9uo66b7smU2xifeA8Wyd/QBTVXWzu7AXL5lRv8Aed" +
+            "MSIXGOFXF88cLZhMpUl1qbKJe5fWKCXguX3vLBX+lDobzk0Ld8jo2V5bhdSzdb3JroHW5iCW5ZgXKDa4Q" +
+            "2g7TFVvJAJ0BiccwH+zYJrIt0ionAY15qmS3GyRCXOVOq83PAYdpgsmv7JUQ7nAsLroF5fko31tDI45/m" +
+            "hgMHnKrlqRSJRtilJsYkQzXT3x6ZHljW4lJG6JlxlurPgD03QiKrZA9I6wt28agYiZx0a9GomvMAZSwID" +
+            "AQAB";
+
     public static final Config CONFIG = getConfig();
+    // 签名方式
+
+
+    static {
+        Factory.setOptions(CONFIG);
+    }
 
     public static Config getConfig() {
 
@@ -21,7 +39,7 @@ public class AlipayConfig {
         config.protocol = "https";
         config.gatewayHost = "openapi.alipaydev.com";
         config.appId = "2016102700770343";
-        config.signType = "RSA2";
+        config.signType = sign_type;
 
         config.alipayPublicKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAhuffPhQ1NxQZX3X8PGt3" +
                 "qIA9Z8wexed2Lrj4ssHvt2fLcjVKr5MQ9GP4hEZG9uo66b7smU2xifeA8Wyd/QBTVXWzu7AXL5lRv8Aed" +
@@ -30,7 +48,7 @@ public class AlipayConfig {
                 "hgMHnKrlqRSJRtilJsYkQzXT3x6ZHljW4lJG6JlxlurPgD03QiKrZA9I6wt28agYiZx0a9GomvMAZSwID" +
                 "AQAB";
         config.merchantPrivateKey = getPrivateKey(config.appId);
-        config.notifyUrl = "http://trdr8s.natappfree.cc/notify_url";
+            config.notifyUrl = " http://mw94bj.natappfree.cc/notify_url";
 
         Factory.setOptions(config);
         return config;
--- src/main/java/com/alipay/alipaydemo/controller/AlipayController.java
+++ src/main/java/com/alipay/alipaydemo/controller/AlipayController.java
@@ -1,5 +1,7 @@
 package com.alipay.alipaydemo.controller;
 
+import com.alipay.alipaydemo.config.AlipayConfig;
+import com.alipay.api.internal.util.AlipaySignature;
 import com.alipay.easysdk.factory.Factory;
 import com.alipay.easysdk.payment.common.models.AlipayTradeCloseResponse;
 import com.alipay.easysdk.payment.common.models.AlipayTradeFastpayRefundQueryResponse;
@@ -11,9 +13,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 @Controller
 public class AlipayController {
@@ -75,8 +82,50 @@ public class AlipayController {
         return "return_url";
     }
 
-    @GetMapping("notify_url")
-    public void callback() {
-        System.out.println("callback notify_url");
+    @RequestMapping("notify_url")
+    public void callback(HttpServletRequest request) throws Exception {
+        Map<String,String> params = new HashMap<String,String>();
+        Map<String,String[]> requestParams = request.getParameterMap();
+        for (Iterator<String> iter = requestParams.keySet().iterator(); iter.hasNext();) {
+            String name = (String) iter.next();
+            String[] values = (String[]) requestParams.get(name);
+            String valueStr = "";
+            for (int i = 0; i < values.length; i++) {
+                valueStr = (i == values.length - 1) ? valueStr + values[i]
+                        : valueStr + values[i] + ",";
+            }
+            //乱码解决，这段代码在出现乱码时使用
+            // valueStr = new String(valueStr.getBytes("ISO-8859-1"), "utf-8");
+            params.put(name, valueStr);
+        }
+
+        boolean signVerified = AlipaySignature.rsaCheckV1(params,AlipayConfig.alipay_public_key,AlipayConfig.charset,AlipayConfig.sign_type);
+        if(signVerified) {//验证成功
+            //商户订单号
+            String out_trade_no = new String(params.get("out_trade_no").getBytes("ISO-8859-1"), "UTF-8");
+
+            //支付宝交易号
+            String trade_no = new String(params.get("trade_no").getBytes("ISO-8859-1"), "UTF-8");
+
+            //交易状态
+            String trade_status = new String(params.get("trade_status").getBytes("ISO-8859-1"), "UTF-8");
+
+            if (trade_status.equals("TRADE_FINISHED")) {
+                //判断该笔订单是否在商户网站中已经做过处理
+                //如果没有做过处理，根据订单号（out_trade_no）在商户网站的订单系统中查到该笔订单的详细，并执行商户的业务程序
+                //如果有做过处理，不执行商户的业务程序
+
+                //注意：
+                //退款日期超过可退款期限后（如三个月可退款），支付宝系统发送该交易状态通知
+            } else if (trade_status.equals("TRADE_SUCCESS")) {
+                //判断该笔订单是否在商户网站中已经做过处理
+                //如果没有做过处理，根据订单号（out_trade_no）在商户网站的订单系统中查到该笔订单的详细，并执行商户的业务程序
+                //如果有做过处理，不执行商户的业务程序
+
+                //注意：
+                //付款完成后，支付宝系统发送该交易状态通知
+            }
+            System.out.println("callback notify_url");
+        }
     }
 }
